c16d5463da45713a9835930241a2ef0e3c39803d
CliffsDover <bottle@dancingbottle.com>
2018-06-23 15:29:52 +0800

    Part 2

---
+++ Part_02/Scenes/Entity.gd
@@ -0,0 +1,30 @@
+extends Label
+
+var displayChar = "@"
+var displayColor = Color( 1, 1, 1, 1 )
+var position = Vector2( 0, 0 )
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+	
+func _updatePosition():
+	rect_position = position
+	
+func _updateDisplayProperties():
+	text = displayChar
+	#add_color_override( "font_color", displayColor )
+	set( "custom_colors/font_color", displayColor )
+
+func Init( _position, _displayChar, _displayColor ):
+	position = _position
+	displayChar = _displayChar
+	displayColor = _displayColor
+	
+	_updateDisplayProperties()
+	_updatePosition()
+	
+func Move( delta ):
+	position += delta
+	_updatePosition()
---
+++ Part_02/Scenes/main.gd
@@ -0,0 +1,46 @@
+extends Node
+
+var grid_size = Vector2( 16, 16 )
+var map_size = Vector2( 50, 32 )
+var entities = []
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+onready var entityScene = preload( "res://Scenes/Entity.tscn" )
+
+func _ready():
+	#$Player.rect_position = Vector2( screen_width / 2 * grid_width, screen_height / 2 * grid_height )
+	set_process_input( true )
+	
+	var player = entityScene.instance()
+	player.name = "player"
+	player.Init( map_size / 2 * grid_size, "@", Color( 1, 1, 1, 1 ) )
+	add_child( player )
+	
+	var npc = entityScene.instance()
+	npc.Init( ( map_size / 2 + Vector2( -5, 0 ) ) * grid_size, "!", Color( 1, 1, 0, 1 ) )
+	add_child( npc )
+	
+	entities.append( player )
+	entities.append( npc )
+	
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed:
+			var delta = Vector2( 0, 0 )
+			if event.scancode == KEY_LEFT:
+				delta = Vector2( -1, 0 )
+			elif event.scancode == KEY_RIGHT:
+				delta = Vector2( 1, 0 )
+			elif event.scancode == KEY_UP:
+				delta = Vector2( 0, -1 )
+			elif event.scancode == KEY_DOWN:
+				delta = Vector2( 0, 1 )
+				
+			$player.Move( delta * grid_size )
